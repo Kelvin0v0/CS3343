281bb4dbd1856ba1915f3944d0c62ac3c04189d1
Kelvin Lau <kelvinlau144@torolab.hk>
2019-09-27 21:26:42 +0800

    setup junit5

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CS3343
 =======
 Welcome to our fake casino for fun! 
+
+How to complie:
+```
+java -cp .\build\libs\CS3343.jar CS3343.Main
+```
---
+++ src/main/java/CS3343/Main.java
@@ -0,0 +1,9 @@
+
+package CS3343;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
+}
