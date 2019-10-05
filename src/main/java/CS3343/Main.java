7225ce929ffc5a7f40e95db2992c06635d3ce715
quintuskilbourn <quintuskilbourn@gmail.com>
2019-10-05 10:04:38 +0800

    Added Roulette functions

--- src/main/java/CS3343/Main.java
+++ src/main/java/CS3343/Main.java
@@ -1,11 +1,28 @@
-
 package CS3343;
-
+import java. Scanner;
 import CS3343.game.Game;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello World");
+        Scanner input = new Scanner(System. in);
+        boolean whileFlag = true;
+        while(whileFlag){
+          System.out.println("Press 1 for Roulette, 'q' to quit");
+          switch(input.nextChar()){
+            case 'q':
+              whileFlag = false;
+              break;
+            case '1':
+              Roulette roulette = new Roulette();
+              roulette.gamePlay();
+          }
+        }
+
+
+
+}
+
 
 /*
         Game game= null;// = new Game();
