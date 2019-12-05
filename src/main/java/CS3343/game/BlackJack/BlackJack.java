bb3737fd251b2e94f3585cfb90ca8f3df2091d83
miketsui3a <42023809+miketsui3a@users.noreply.github.com>
2019-12-05 18:49:45 +0800

    Update BlackJack.java
    commit

--- src/main/java/CS3343/game/BlackJack/BlackJack.java
+++ src/main/java/CS3343/game/BlackJack/BlackJack.java
@@ -25,6 +25,53 @@ public class BlackJack {
 		return this.player;
 	}
 	
+	public Deck getPlayerDeck() {
+		return this.playerDeck;
+	}
+	public Deck getCpuDeck() {
+		return this.cpuDeck;
+	}
+	public void setPlayerDeck(Deck deck) {
+		this.playerDeck = deck;
+	}
+	public void setCpuDeck(Deck deck) {
+		this.cpuDeck = deck;
+	}
+	
+//	public void gameStart2() {
+//		System.out.println("    /$$   /$$$$$$$ /$$                  /$$         /$$$$$                  /$$         /$$   \n" + 
+//				"  /$$$$$$| $$__  $| $$                 | $$        |__  $$                 | $$       /$$$$$$ \n" + 
+//				" /$$__  $| $$  \\ $| $$ /$$$$$$  /$$$$$$| $$   /$$     | $$ /$$$$$$  /$$$$$$| $$   /$$/$$__  $$\n" + 
+//				"| $$  \\__| $$$$$$$| $$|____  $$/$$_____| $$  /$$/     | $$|____  $$/$$_____| $$  /$$| $$  \\__/\n" + 
+//				"|  $$$$$$| $$__  $| $$ /$$$$$$| $$     | $$$$$$/ /$$  | $$ /$$$$$$| $$     | $$$$$$/|  $$$$$$ \n" + 
+//				" \\____  $| $$  \\ $| $$/$$__  $| $$     | $$_  $$| $$  | $$/$$__  $| $$     | $$_  $$ \\____  $$\n" + 
+//				" /$$  \\ $| $$$$$$$| $|  $$$$$$|  $$$$$$| $$ \\  $|  $$$$$$|  $$$$$$|  $$$$$$| $$ \\  $$/$$  \\ $$\n" + 
+//				"|  $$$$$$|_______/|__/\\_______/\\_______|__/  \\__/\\______/ \\_______/\\_______|__/  \\__|  $$$$$$/\n" + 
+//				" \\_  $$_/                                                                            \\_  $$_/ \n" + 
+//				"   \\__/                                                                                \\__/   \n" + 
+//				"                                                                                             ");
+//		System.out.println("Welcom ! "+player.getName());
+//		System.out.println("Your Balance: "+player.getBalance());
+//		System.out.print("Want to win some cash??? (Y/N)");
+//		
+//		char isPlay = this.inGame.next().charAt(0);
+//		
+//		while(isPlay!='Y'&&isPlay!='N') {
+//			System.out.println("Invalid input. Please re-enter: ");
+//		}
+//		
+//		if(isPlay=='N') {
+//			System.out.println("Bye!");
+//			return;
+//		}
+//		
+//		playerDeck.dealCard();
+//		playerDeck.dealCard();
+//		cpuDeck.dealCard();
+//		cpuDeck.dealCard();
+//		
+//	}
+	
 	public void gameInit() {
 		int quitFlag = 0;
 		System.out.println("    /$$   /$$$$$$$ /$$                  /$$         /$$$$$                  /$$         /$$   \n" + 
@@ -38,7 +85,7 @@ public class BlackJack {
 				" \\_  $$_/                                                                            \\_  $$_/ \n" + 
 				"   \\__/                                                                                \\__/   \n" + 
 				"                                                                                             ");
-		System.out.println("Welcome ! "+player.getName());
+		System.out.println("Welcome! "+player.getName());
 		System.out.println("Your Balance: "+player.getBalance());
 		System.out.print("Want to win some cash??? (Y/N)");
 		
@@ -66,14 +113,22 @@ public class BlackJack {
 		do {
 			System.out.print("Place your Bet: ");
 			this.betValue =  this.betScanner.nextInt();
-			if(this.betValue>this.player.getBalance()) {
+			if(!checkBalance(this.betValue,this.player.getBalance())) {
 				System.out.println("You don't have enough money.");
 			}
-		}while(this.betValue>this.player.getBalance());
+		}while(!checkBalance(this.betValue,this.player.getBalance()));
 		this.betAccumunate += this.betValue; 
-		this.getPlayer().setBalance(this.getPlayer().getBalance()-this.betValue);
+		this.getPlayer().bet(betValue);
 		System.out.println("Total bet Value: "+this.betAccumunate);
+		System.out.println("Player's balance: "+this.getPlayer().getBalance());
 		this.roundCnt++;
+	}
+	
+	public boolean checkBalance(int bet, int balance) {
+		if(bet>balance) {
+			return false;
+		}
+		return true;
 	}
 	
 	public void askAction() {
@@ -84,6 +139,7 @@ public class BlackJack {
 		switch(action) {
 			case 'B': {
 				this.inputBet();
+				break;
 			}
 			case 'H': {
 				playerDeck.dealCard();
@@ -95,28 +151,59 @@ public class BlackJack {
 				}
 				cpuDeck.printAllCpuCard();
 				playerDeck.printCard();
-				if (cpuDeck.calCardSum()<22 && cpuDeck.calCardSum()>playerDeck.calCardSum()) {
-					System.out.println("You Losed !");
-					System.out.println("Your balance: "+this.getPlayer().getBalance());
-					this.playing = 0;
-					return;
-				}else if(cpuDeck.calCardSum()>21) {
-					System.out.println("Cpu busted! You win !");
-					this.getPlayer().setBalance(this.getPlayer().getBalance()+betAccumunate*2);
-					System.out.println("Your balance: "+this.getPlayer().getBalance());
-					this.playing = 0;
-					return;
-				}else if(cpuDeck.calCardSum()==playerDeck.calCardSum()) {
-					System.out.println("Draw!");
-					this.playing = 0;
-					return;
+
+				switch(checkGameCondition()) {
+					case 0:{
+						System.out.println("You Busted!");
+						System.out.println("Your balance: "+this.getPlayer().getBalance());
+						break;
+					}
+					case 1:{
+						System.out.println("You Losed !");
+						System.out.println("Your balance: "+this.getPlayer().getBalance());
+						this.playing = 0;
+						break;
+					}
+					case 2:{
+						System.out.println("Cpu busted! You win !");
+						this.getPlayer().setBalance(betAccumunate*2);
+						System.out.println("Your balance: "+this.getPlayer().getBalance());
+						this.playing = 0;
+						break;
+					}
+					case 3:{
+						System.out.println("Draw!");
+						this.playing = 0;
+						this.getPlayer().setBalance(betAccumunate);
+						break;
+					}
+					case 4:{
+						System.out.println("You are bigger then cpu, you win.");
+						this.playing = 0;
+						this.getPlayer().setBalance(betAccumunate*2);
+						break;
+					}
 				}
-				break;
+				
+				return; 
 			}
 			default: System.out.println("Something wrong...");
 			this.playing = 0;
 			return;
 		}
+	}
+	
+	public int checkGameCondition() {
+		if(playerDeck.calCardSum()>21) {
+			return 0; //player busted
+		}else if(cpuDeck.calCardSum()<22 && cpuDeck.calCardSum()>playerDeck.calCardSum()) {
+			return 1; //cpu win
+		}else if(cpuDeck.calCardSum()>21) {
+			return 2; //cpu busted
+		}else if(cpuDeck.calCardSum()==playerDeck.calCardSum()) {
+			return 3; //draw
+		}
+		return 4; //you win
 	}
 	
 	public int gameStart() {
@@ -133,7 +220,7 @@ public class BlackJack {
 				cpuDeck.printCpuCard();
 				playerDeck.printCard();
 			}else {
-				if (playerDeck.calCardSum()>21) {
+				if (checkGameCondition()==0) {
 					playerDeck.printCard();
 					System.out.println("Busted !!!");
 					playing=0;
